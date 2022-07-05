0ed80147f46953da656f715a3f6f875543bf3c00
yunjae <yunjae.oh.nl@gmail.com>
2022-07-05 20:18:21 +0200

    build: add docker-compose

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,4 +1,4 @@
 !#bin/bash
 
-docker build -t app ./app/. --network=host  # Use host network to prevent issue when running in VPN
-docker build -t server ./server/. --network=host
+docker build -t app ./app/. --no-cache --network=host 
+docker build -t server ./server/. --no-cache --network=host
