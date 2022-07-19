fb43445710ec8eb69b30603a6e9ab06c14ffe163
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-07-19 17:00:18 +0200

    build: render comment list with props

--- app/src/components/TaskCard.tsx
+++ app/src/components/TaskCard.tsx
@@ -1,31 +1,46 @@
 import * as React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { getLanguage } from '../utils';
 
-const TaskCard: React.FC = () => {
+interface TaskCardProps {
+  title: string;
+  commentStyle: string;
+  type: string;
+  fullComment: string[];
+  path: string;
+  lineNumber: number;
+}
+
+const TaskCard: React.FC<TaskCardProps> = ({ title, type, fullComment, path, lineNumber }: TaskCardProps) => {
+  const codeStrings = fullComment.join('\n');
+  const language = getLanguage(path);
+
   return (
-    <div className="w-[560px] h-auto border-0 border-dolist-lightgray rounded m-auto mt-14 mb-14 bg-dolist-cream">
+    <div className="w-[680px] h-auto border-0 border-dolist-lightgray rounded m-auto mt-14 mb-14 bg-dolist-cream">
       <div className="w-5/6 h-full m-auto pt-3 pb-3">
         <div className="pt-4 pb-2">
-          <h3 className="text-lg font-bold">Add typing to createPerson method</h3>
+          <h3 className="text-lg font-bold">{`${title}`}</h3>
         </div>
         <div className="flex flex-row justify-between pt-5 pb-5">
           <div className="text-left">
-            <p className="text-xs">Type: TODO</p>
+            <p className="text-xs">{`Type: ${type}`}</p>
           </div>
           <div className="text-right">
-            <p className="text-xs">Path: /src/input/example.py</p>
-            <p className="text-xs">Line: 18</p>
+            <p className="text-xs">{`Path: ${path}`}</p>
+            <p className="text-xs">{`Line: ${lineNumber}`}</p>
           </div>
         </div>
         <div className="pb-3">
           <SyntaxHighlighter
             showLineNumbers
             wrapLongLines
-            startingLineNumber={18}
-            language="javascript"
+            startingLineNumber={lineNumber}
+            language={language}
             style={nightOwl}
-          >{`// TODO: comment`}</SyntaxHighlighter>
+          >
+            {codeStrings}
+          </SyntaxHighlighter>
         </div>
       </div>
     </div>
--- app/tests/TaskCard/TaskCard.snapshot.test.tsx
+++ app/tests/TaskCard/TaskCard.snapshot.test.tsx
@@ -3,8 +3,26 @@ import * as renderer from 'react-test-renderer';
 import { TaskCard } from '../../src/components';
 
 describe('Snapshot testing of TaskCard component', () => {
+  const testData = {
+    type: 'TODO',
+    commentStyle: 'oneline',
+    title: 'This should be parsed',
+    fullComment: ['// TODO: This should be parsed'],
+    path: '/Users/yunjae/Desktop/engineering/comment-parser/input/folder_b/folder_b.ts',
+    lineNumber: 1,
+  };
+
   test('renders correctly', () => {
-    const component = renderer.create(<TaskCard />);
+    const component = renderer.create(
+      <TaskCard
+        type={testData.type}
+        title={testData.title}
+        commentStyle={testData.commentStyle}
+        fullComment={testData.fullComment}
+        path={testData.path}
+        lineNumber={testData.lineNumber}
+      />,
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
