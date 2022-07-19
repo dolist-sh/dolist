8243f82337993cdb7ac142b131f296cb83e0e0e5
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-07-19 16:59:31 +0200

    build: render comment list with props

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import json_data from '../.data/output-2022-7-18-1658164458.json';
 import { TaskCard } from '../components';
 
 const Homepage: NextPage = () => {
+  console.log(json_data);
+
   return (
     <div className="flex flex-col w-11/12 m-auto">
-      <TaskCard />
+      {json_data.map((comment, index) => (
+        <TaskCard
+          key={index}
+          type={comment.type}
+          title={comment.title}
+          commentStyle={comment.commentStyle}
+          fullComment={comment.fullComment}
+          path={comment.path}
+          lineNumber={comment.lineNumber}
+        />
+      ))}
     </div>
   );
 };
