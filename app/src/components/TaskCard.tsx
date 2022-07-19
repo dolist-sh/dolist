9e58fc921014d94f7fb12511ff7c5004c61f2016
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-07-19 15:47:41 +0200

    chore: style adjustment

--- app/src/components/TaskCard.tsx
+++ app/src/components/TaskCard.tsx
@@ -4,9 +4,9 @@ import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const TaskCard: React.FC = () => {
   return (
-    <div className="w-[560px] h-auto border-0 border-dolist-lightgray rounded m-auto mt-8 mb-8 bg-dolist-cream">
-      <div className="w-11/12 h-full m-auto pt-3 pb-3">
-        <div className="pt-2 pb-2">
+    <div className="w-[560px] h-auto border-0 border-dolist-lightgray rounded m-auto mt-14 mb-14 bg-dolist-cream">
+      <div className="w-5/6 h-full m-auto pt-3 pb-3">
+        <div className="pt-4 pb-2">
           <h3 className="text-lg font-bold">Add typing to createPerson method</h3>
         </div>
         <div className="flex flex-row justify-between pt-5 pb-5">
@@ -18,9 +18,10 @@ const TaskCard: React.FC = () => {
             <p className="text-xs">Line: 18</p>
           </div>
         </div>
-        <div>
+        <div className="pb-3">
           <SyntaxHighlighter
             showLineNumbers
+            wrapLongLines
             startingLineNumber={18}
             language="javascript"
             style={nightOwl}
