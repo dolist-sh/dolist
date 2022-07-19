54bf68662107cd006604577e648ae81d6aa8456c
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-07-19 15:42:44 +0200

    build: add code block section'

--- app/src/components/TaskCard.tsx
+++ app/src/components/TaskCard.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const TaskCard: React.FC = () => {
   return (
-    <div className="w-[560px] h-auto border-0 border-dolist-lightgray rounded m-auto mt-5 mb-5 bg-dolist-cream">
+    <div className="w-[560px] h-auto border-0 border-dolist-lightgray rounded m-auto mt-8 mb-8 bg-dolist-cream">
       <div className="w-11/12 h-full m-auto pt-3 pb-3">
         <div className="pt-2 pb-2">
           <h3 className="text-lg font-bold">Add typing to createPerson method</h3>
@@ -16,7 +18,14 @@ const TaskCard: React.FC = () => {
             <p className="text-xs">Line: 18</p>
           </div>
         </div>
-        <div>Code block here</div>
+        <div>
+          <SyntaxHighlighter
+            showLineNumbers
+            startingLineNumber={18}
+            language="javascript"
+            style={nightOwl}
+          >{`// TODO: comment`}</SyntaxHighlighter>
+        </div>
       </div>
     </div>
   );
