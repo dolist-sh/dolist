dcb6e836bf591f09a8372805958cc7a287479994
yunjae <yunjae.oh.nl@gmail.com>
2022-07-25 15:36:41 +0200

    build: add git auth at docker build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+GIT_ACCESS_TOKEN=$1 
+
 docker build -t app ./app/. --no-cache --network=host 
-docker build -t server ./server/. --no-cache --network=host
+docker build --build-arg GIT_ACCESS_TOKEN="${GIT_ACCESS_TOKEN}" -t server ./server/. --no-cache --network=host
 
 exit 0
