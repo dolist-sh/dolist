b9c66ef7e7032b1e4e2f571af7c46902cf6a2bb4
yunjae <yunjae.oh.nl@gmail.com>
2022-08-08 16:10:02 +0200

    build: apply color and add spinner

--- app/src/pages/process_auth.tsx
+++ app/src/pages/process_auth.tsx
@@ -1,7 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useState, useEffect, CSSProperties } from 'react';
+import { GlobalContext } from '../contexts/global';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { getAuthCode } from '../api';
+import PanmanLoader from 'react-spinners/PacmanLoader';
+
+const Spinner: React.FC = () => {
+  const [spinnerColor, setSpinnerColor] = useState(null);
+
+  const globalcontext = useContext(GlobalContext);
+  const override: CSSProperties = {
+    display: 'block',
+    margin: '0 auto',
+  };
+
+  useEffect(() => {
+    if (globalcontext.theme === 'dark') {
+      setSpinnerColor('#E8E7E0');
+    }
+    if (globalcontext.theme === 'light') {
+      setSpinnerColor('#1E2B3D');
+    }
+  }, [globalcontext.theme]);
+
+  return (
+    <>
+      <PanmanLoader loading={true} size={30} color={spinnerColor} cssOverride={override} />
+    </>
+  );
+};
 
 const ProcessAuthPage: NextPage = () => {
   const { query, push } = useRouter();
@@ -23,10 +50,13 @@ const ProcessAuthPage: NextPage = () => {
   }, [query]);
 
   return (
-    <div className="flex flex-col justify-center w-11/12 h-screen m-auto">
-      <div className="flex flex-row justify-center align-middle">
-        <p>{'Process Auth'}</p>
+    <div className="bg-dolist-bg-light dark:bg-dolist-bg-dark flex flex-col justify-center w-11/12 h-screen m-auto">
+      <div className="w-full h-20 content-center">
+        <Spinner />
       </div>
+      <p className="w-full text-center pl-6 text-dolist-darkblue dark:text-dolist-cream">
+        {'Busy processing, hang tight..'}
+      </p>
     </div>
   );
 };
