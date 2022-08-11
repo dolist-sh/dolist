df5796294e4cf8d3331b34ada7ab1592dd418bb2
yunjae <yunjae.oh.nl@gmail.com>
2022-08-11 15:22:25 +0200

    build: fetch data at the modal and css change

--- app/src/components/AddRepoModal.tsx
+++ app/src/components/AddRepoModal.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Fragment, useState, useEffect } from 'react';
 import { Transition } from '@headlessui/react';
 import { XIcon } from '@heroicons/react/outline';
+import { getUserRepos } from '../api';
 import RepoCard from './RepoCard';
 
 interface AddRepoModalProps {
@@ -12,14 +13,25 @@ interface AddRepoModalProps {
 
 const AddRepoModal: React.FC<AddRepoModalProps> = ({ openCounter, githubLogoUri }: AddRepoModalProps) => {
   const [open, setOpen] = useState(false);
+  const [repos, setRepos] = useState(null);
 
   useEffect(() => {
     openCounter > 0 ? setOpen(!open) : null;
   }, [openCounter]);
 
+  useEffect(() => {
+    const token = localStorage.getItem('token') as string;
+
+    if (open) {
+      getUserRepos(token).then((data) => {
+        setRepos(data);
+      });
+    }
+  }, [open]);
+
   return (
     <Transition.Root show={open} as={Fragment}>
-      <div onClick={() => setOpen(false)} className="relative z-10">
+      <div className="relative z-10">
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -29,69 +41,64 @@ const AddRepoModal: React.FC<AddRepoModalProps> = ({ openCounter, githubLogoUri 
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div
-            onClick={() => setOpen(false)}
-            className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity z-[100]"
-          />
+          <div onClick={() => setOpen(false)} className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
-
-        <div className="fixed z-[120] inset-0 overflow-y-auto">
-          <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <div className="relative bg-white dark:bg-dolist-bg-dark rounded px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
-                <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
-                  <button
-                    type="button"
-                    className="bg-white rounded-md text-black dark:text-white focus:outline-none"
-                    onClick={() => setOpen(false)}
-                  >
-                    <span className="sr-only">Close</span>
-                    <XIcon className="h-6 w-6 bg-white dark:bg-dolist-bg-dark" aria-hidden="true" />
-                  </button>
+        <div className="fixed flex left-[50%] translate-x-[-50%]">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="relative bg-white dark:bg-dolist-bg-dark rounded px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
+              <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                <button
+                  type="button"
+                  className="bg-white rounded-md text-black dark:text-white focus:outline-none"
+                  onClick={() => setOpen(false)}
+                >
+                  <span className="sr-only">Close</span>
+                  <XIcon className="h-6 w-6 bg-white dark:bg-dolist-bg-dark" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="sm:flex sm:flex-col sm:items-start">
+                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <h3 className="text-lg leading-6 font-std font-bold text-black dark:text-white">
+                    {`Add Repositories`}
+                  </h3>
                 </div>
-                <div className="sm:flex sm:flex-col sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 className="text-lg leading-6 font-std font-bold text-black dark:text-white">
-                      {`Add Repositories`}
-                    </h3>
+                <div className="flex flex-col w-[95%] ml-4 mt-7 border-b border-dolist-lightgray dark:border-dolist-cream">
+                  <div className="flex flex-row pb-1">
+                    <p className="cursor-pointer text-xs font-std font-bold text-dolist-gray dark:text-dolist-cream pr-3">{`whathecker`}</p>
+                    <p className="cursor-pointer text-xs font-std hover:font-bold text-dolist-gray dark:text-dolist-cream pr-3">{`username_1`}</p>
                   </div>
-                  <div className="flex flex-col w-[95%] ml-4 mt-7 border-b border-dolist-lightgray dark:border-dolist-cream">
-                    <div className="flex flex-row pb-1">
-                      <p className="cursor-pointer text-xs font-std font-bold text-dolist-gray dark:text-dolist-cream pr-3">{`whathecker`}</p>
-                      <p className="cursor-pointer text-xs font-std hover:font-bold text-dolist-gray dark:text-dolist-cream pr-3">{`username_1`}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col w-[95%] ml-4 mt-4 min-h-[200px] max-h-[250px] overflow-hidden">
-                  <div className="flex flex-col w-full overflow-y-scroll">
-                    <RepoCard fullName="whathecker/api" githubLogoUri={githubLogoUri} />
-                    <RepoCard fullName="whathecker/api" githubLogoUri={githubLogoUri} />
-                    <RepoCard fullName="whathecker/api" githubLogoUri={githubLogoUri} />
-                    <RepoCard fullName="whathecker/api" githubLogoUri={githubLogoUri} />
-                    <RepoCard fullName="whathecker/api" githubLogoUri={githubLogoUri} />
-                    <RepoCard fullName="whathecker/api" githubLogoUri={githubLogoUri} />
-                  </div>
-                </div>
-                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                  <button
-                    type="button"
-                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-dolist-darkblue dark:bg-dolist-green text-base font-std font-bold text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => setOpen(false)}
-                  >
-                    Confirm
-                  </button>
                 </div>
               </div>
-            </Transition.Child>
-          </div>
+              <div className="flex flex-col w-[95%] ml-4 mt-4 min-h-[200px] max-h-[250px] overflow-hidden">
+                <div className="flex flex-col w-full overflow-y-scroll">
+                  {/** TODO: Add Loader */}
+                  {repos
+                    ? repos.map((repo, index) => {
+                        return <RepoCard key={index} fullName={repo.full_name} githubLogoUri={githubLogoUri} />;
+                      })
+                    : null}
+                  {}
+                </div>
+              </div>
+              <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-dolist-darkblue dark:bg-dolist-green text-base font-std font-bold text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => setOpen(false)}
+                >
+                  Confirm
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
         </div>
       </div>
     </Transition.Root>
