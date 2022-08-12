de551a6ccd31a107f9ccf3da702b012e6d72c28a
yunjae <yunjae.oh.nl@gmail.com>
2022-08-12 09:59:06 +0200

    build: schema of monitoredrepo

--- server/src/storage/model.py
+++ server/src/storage/model.py
@@ -21,5 +21,19 @@ user_schema = Table(
     Column("createdAt", Integer, nullable=False),
 )
 
+monitoredrepo_schema = Table(
+    "monitoredrepo",
+    metadata_obj,
+    Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4()),
+    Column("fullName", string(50), nullable=False),
+    Column("userId", UUID(as_uuid=True), nullable=False),
+    Column("provider", string(30), nullable=False),
+    Column("status", string(20), nullable=False),
+    Column("createdAt", Integer, nullable=False),
+    Column("lastUpdated", Integer, nullable=False),
+)
+
+
 # Create a new table if doens't exist
 user_schema.create(engine, checkfirst=True)
+monitoredrepo_schema.create(engine, checkfirst=True)
