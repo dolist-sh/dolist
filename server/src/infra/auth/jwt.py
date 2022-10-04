88d70dc40f686d6eee876f09f06989f3c3d69d65
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-10-04 10:39:58 +0200

    chore: refactor jwt handler to class and write tests

--- server/src/infra/auth/jwt.py
+++ server/src/infra/auth/jwt.py
@@ -1,31 +1,38 @@
 import jwt
-from datetime import datetime, timedelta, timezone
+
 from app.domain.auth import CreateMachineTokenInput, MachineToken
+from datetime import datetime, timedelta, timezone
 from config import JWT_SECRET
 
 
-def issue_token(email: str) -> str:
-    payload = {
-        "email": email,
-        "exp": datetime.now(tz=timezone.utc) + timedelta(days=30),
-    }
-    encoded = jwt.encode(payload, JWT_SECRET, algorithm="HS256")
+class JWTAdaptor:
+    def __init__(self, jwt: jwt) -> None:
+        """
+        Taking jwt as injected-dependency as it is an external package.
+        """
+        self.jwt = jwt
 
-    return encoded
+    def issue_token(self, email: str) -> str:
+        payload = {
+            "email": email,
+            "exp": datetime.now(tz=timezone.utc) + timedelta(days=30),
+        }
+        encoded = jwt.encode(payload, JWT_SECRET, algorithm="HS256")
 
+        return encoded
 
-def issue_machine_token(input: CreateMachineTokenInput) -> MachineToken:
-    exp_at = datetime.now(tz=timezone.utc) + timedelta(days=30)
+    def issue_machine_token(self, input: CreateMachineTokenInput) -> MachineToken:
+        exp_at = datetime.now(tz=timezone.utc) + timedelta(days=30)
 
-    payload = {
-        "aud": input["audience"],
-        "iss": "dolist_server",
-        "client_id": input["client_id"],
-        "client_secret": input["client_secret"],
-        "exp": exp_at,
-    }
-    access_token = jwt.encode(payload, JWT_SECRET, algorithm="HS256")
+        payload = {
+            "aud": input["audience"],
+            "iss": "dolist_server",
+            "client_id": input["client_id"],
+            "client_secret": input["client_secret"],
+            "exp": exp_at,
+        }
+        access_token = jwt.encode(payload, JWT_SECRET, algorithm="HS256")
 
-    return MachineToken(
-        token_type="Bearer", access_token=access_token, expires_at=exp_at
-    )
+        return MachineToken(
+            token_type="Bearer", access_token=access_token, expires_at=exp_at
+        )
--- server/tests/infra/auth/test_jwt.py
+++ server/tests/infra/auth/test_jwt.py
@@ -1,2 +1,27 @@
-def test_jwt():
-    assert 1 == 1
+import pytest
+
+from src.infra.auth.jwt import JWTAdaptor, CreateMachineTokenInput, MachineToken, jwt
+
+
+@pytest.fixture
+def jwt_adaptor():
+    return JWTAdaptor(jwt)
+
+
+def test_issue_token(jwt_adaptor: JWTAdaptor):
+    token = jwt_adaptor.issue_token("awesome_user@gmail.com")
+
+    assert (type(token) is str) == True
+
+
+def test_issue_machine_token(jwt_adaptor: JWTAdaptor):
+    payload: CreateMachineTokenInput = {
+        "audience": "worker",
+        "grant_type": "client_credentials",
+        "client_id": "009ca4b55b89633cd79f930d71df2d846d699d2d",
+        "client_secret": "491b15b6b08f721376c22e06e640b5126d86da58",
+    }
+
+    machine_token = jwt_adaptor.issue_machine_token(payload)
+    
+    assert (type(machine_token) is MachineToken) == True
