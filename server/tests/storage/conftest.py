e610e9e01505b6b608b04553a52858882184b149
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-10-05 16:01:01 +0200

    chore: prepopulate test data at db test

--- server/tests/storage/conftest.py
+++ server/tests/storage/conftest.py
@@ -12,16 +12,22 @@ from src.infra.storage.model import (
 from src.infra.storage.mrepodb import MonitoredRepoDBAdaptor
 from src.infra.storage.userdb import UserDBAdaptor
 
+from .helpers import generate_test_user_dataset, generate_test_mrepo_dataset
 from config import DB_HOST, DB_USER, DB_PWD
 
 
 @pytest.fixture
-def test_dataset():
-    pass
+def test_user_dataset():
+    return generate_test_user_dataset(10)
 
 
 @pytest.fixture
-def test_db_session():
+def test_mrepo_dataset():
+    return generate_test_mrepo_dataset(5)
+
+
+@pytest.fixture
+def test_db_session(test_user_dataset, test_mrepo_dataset):
 
     engine = create_engine(f"postgresql://{DB_USER}:{DB_PWD}@{DB_HOST}:5432/testdb")
 
@@ -31,6 +37,10 @@ def test_db_session():
     user_schema.create(engine, checkfirst=True)
     monitored_repo_schema.create(engine, checkfirst=True)
     parsed_comment_schema.create(engine, checkfirst=True)
+
+    # Prepopulate DB with test dataset
+    engine.execute(user_schema.insert(), test_user_dataset)
+    engine.execute(monitored_repo_schema.insert(), test_mrepo_dataset)
 
     # Use yield instead of return to start the teardown process.
     yield engine
