6a8bbc23cee1a82be24799f03f291021db8545ec
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-10-10 11:30:35 +0200

    chore: correct the mistake at prev fix

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 -host="localhost" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE dolistdb;
 EOSQL
