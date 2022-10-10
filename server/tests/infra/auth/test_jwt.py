7a7be6e5a15285b8b5e2ef121d3474333241d0e6
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-10-10 14:36:35 +0200

    fix: try exporting env var at job command

--- server/tests/infra/auth/test_jwt.py
+++ server/tests/infra/auth/test_jwt.py
@@ -2,26 +2,26 @@ import pytest
 
 from src.infra.auth.jwt import JWTService, CreateMachineTokenInput, MachineToken, jwt
 
-
 @pytest.fixture
-def jwt_adaptor():
+def jwt_service():
     return JWTService(jwt)
 
 
-def test_issue_token(jwt_adaptor: JWTService):
-    token = jwt_adaptor.issue_token("awesome_user@gmail.com")
+def test_issue_token(jwt_service: JWTService):
+    token = jwt_service.issue_token("awesome_user@gmail.com")
 
     assert (type(token) is str) == True
-
-
-def test_issue_machine_token(jwt_adaptor: JWTService):
+    
+    
+def test_issue_machine_token(jwt_service: JWTService):
     payload: CreateMachineTokenInput = {
         "audience": "worker",
         "grant_type": "client_credentials",
         "client_id": "009ca4b55b89633cd79f930d71df2d846d699d2d",
         "client_secret": "491b15b6b08f721376c22e06e640b5126d86da58",
     }
-
-    machine_token = jwt_adaptor.issue_machine_token(payload)
+    machine_token = jwt_service.issue_machine_token(payload)
 
     assert (type(machine_token) is MachineToken) == True
+
+        
